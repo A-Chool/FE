8d27d5aa3a4cfa68be778680c93e66188de97dcc
hobit22 <hobit22@naver.com>
2022-05-08 16:05:01 +0900

    충돌 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://3.39.0.208",
-=======
-  baseURL: "http://3.39.0.208/",
->>>>>>> ca015235350c5f5609b2c1c6dbf62c1eaac14453
+  baseURL: "http://3.39.0.208/"
 });
 export default api;
