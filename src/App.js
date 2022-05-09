acd6eb2ace385a51a8ec8c6abdfcecbb91fab121
kyngmn <kmoffi@naver.com>
2022-05-10 02:52:22 +0900

    feat : 채팅 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import { Route } from "react-router-dom";
 import UserTeamBoard from "./pages/user/UserTeamBoard";
 import UserCheckIn from "./pages/user/UserCheckIn";
 
-import AdminTeamPage from './pages/admin/AdminTeamPage';
-import AdminUserPage from './pages/admin/AdminUserPage';
+import AdminTeamPage from "./pages/admin/AdminTeamPage";
+import AdminUserPage from "./pages/admin/AdminUserPage";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -18,31 +18,34 @@ import AdminLogin from "./pages/AdminLogin";
 import KakaoOauth from "./shared/KakaoOauth";
 import ChatingPage from "./pages/ChatingPage";
 // import ChatRoom from "./pages/ChatRoom";
+
+import ChatContainer from "./components/chat/ChatContainer";
+
 function App() {
   return (
-    <React.Fragment>
-      <ContentWrap>
-        <ConnectedRouter history={history}>
-          <Route path="/" exact component={Login} />
-          <Route path="/adminlogin" exact component={AdminLogin} />
-          <Route path="/signup" exact component={Signup} />
-          <Route path="/api/user/kakao/callback" exact component={KakaoOauth} />
-          <Route path="/chat" exact component={ChatingPage} />
-          <Route path="/AdminUserPage" exact component={AdminUserPage}></Route>
-          <Route path="/AdminTeamPage" exact component={AdminTeamPage}></Route>
-          <Route path="/UserCheckIn" exact component={UserCheckIn}></Route>
-          <Route path="/UserTeamBoard" exact component={UserTeamBoard}></Route>
-        </ConnectedRouter>
-      </ContentWrap>
-    </React.Fragment>
+    <ContentWrap>
+      <ChatContainer />
+      <ConnectedRouter history={history}>
+        <Route path="/" exact component={Login} />
+        <Route path="/adminlogin" exact component={AdminLogin} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/api/user/kakao/callback" exact component={KakaoOauth} />
+        <Route path="/chat" exact component={ChatingPage} />
+        <Route path="/AdminUserPage" exact component={AdminUserPage}></Route>
+        <Route path="/AdminTeamPage" exact component={AdminTeamPage}></Route>
+        <Route path="/UserCheckIn" exact component={UserCheckIn}></Route>
+        <Route path="/UserTeamBoard" exact component={UserTeamBoard}></Route>
+      </ConnectedRouter>
+    </ContentWrap>
   );
 }
 
 const ContentWrap = styled.div`
   margin: 0 auto;
   max-width: 100%;
-  max-height : 100%;
+  max-height: 100%;
   box-sizing: border-box;
-`
+  position: relative;
+`;
 
 export default App;
