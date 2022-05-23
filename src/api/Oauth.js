9653ac7f6dfc20c76c998a04eec3b43bfca0938c
이경태 <leekyeongtae@gyeongbongs-macbook.local>
2022-05-23 23:24:56 +0900

    kakaotest

--- src/api/Oauth.js
+++ src/api/Oauth.js
@@ -1,4 +1,4 @@
 // const redirect_uri = "http://localhost:3000/api/user/kakao/callback";
-const redirect_uri = "https://a-chool.com/api/user/kakao/callback";
+const redirect_uri = "https://achool.shop/api/user/kakao/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${redirect_uri}&response_type=code`;
