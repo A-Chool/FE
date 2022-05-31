0c370cb60096fcfb18511fac7ad329de9aece031
kyngmn <kmoffi@naver.com>
2022-06-01 03:07:08 +0900

    test

--- src/api/Oauth.js
+++ src/api/Oauth.js
@@ -5,5 +5,5 @@ const naver_redirect_uri = "https://www.a-chool.com/api/user/naver/callback";
 
 // 배포한 주소로
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${kakao_redirect_uri}&response_type=code`;
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${naver_redirect_uri}&response_type=code&state=STATE_STRING`;
+export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${naver_redirect_uri}&state=STATE_STRING&response_type=code`;
 // export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/token?client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&client_secret=${process.env.REACT_APP_NAVER_CLIENT_SECRET}&redirect_uri=${naver_redirect_uri}&grant_type=authorization_code&response_type=token&`;
